74c954a61988a8b364b07c6f404a879fcbefde8f
StefanOffice <sjankovicdev@gmail.com>
2022-03-10 13:25:36 +0100

    [Project started]

---
+++ mainmodule/src/main/java/com/app/caloriescounterapp/CaloriesCounterAppApplication.java
@@ -0,0 +1,13 @@
+package com.app.caloriescounterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaloriesCounterAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaloriesCounterAppApplication.class, args);
+    }
+
+}
